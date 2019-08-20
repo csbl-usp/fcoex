a2b732177ec87bfa14ac6c17276827d5e020bda0
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-08-20 14:36:24 -0300

    Adding mini dataset for vignette and examples

---
+++ R/datasets.R
@@ -0,0 +1,22 @@
+#' Processed subset of the pbmc3k dataset from PBMC genomics
+#'
+#' A subset with 600 sampled cells and the top 1700 variable 
+#' genes from the  TENxPBMCData package pbmc3k dataset. 
+#' 
+#' Preprocessed in accordance to OSCA (August 2019, https://osca.bioconductor.org/)
+#' 
+#' scater::normalized .
+#' PCA with 50 components.
+#' snn graph on the PCA space + louvain clustering to yield 8 clusters . 
+#' UMAP already ran 
+#'
+#' @name mini_pbmc3k 
+#' @docType data
+#' @usage data(mini_pbmc3k)
+#' @format An object of class \code{SingleCellExperiment}
+#' @keywords datasets
+#' @source \href{https://bioconductor.org/packages/release/data/experiment/html/TENxPBMCData.html}
+#' @examples
+#' data(mini_pbmc3k)
+#' 
+"mini_pbmc3k"
